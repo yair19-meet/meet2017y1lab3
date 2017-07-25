9a54db5f9020642282b5d6088ec5f530f147ae82
yair19-meet <yair19@meet.mit.edu>
2017-07-25 10:08:15 +0300

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+
+print("Γειά σου Κόσμε")
+print("Ola Mundo")
+print("Ahoj světe")
+print("안녕 세상")
+print("你好，世界")
+print("नमस्ते दुनिया")
+print("salamu , Dunia")
---
+++ RobertFrost.py
@@ -0,0 +1,10 @@
+twoRoads = "Two roads diverged in a wood, and I-"
+#roadTaken = "I took the one most traveled by,"
+less = "I took the one less traveled by,"
+theDifference = "And that has made all the difference."
+
+roadTaken = less
+
+print(twoRoads)
+print(roadTaken)
+print(theDifference)
